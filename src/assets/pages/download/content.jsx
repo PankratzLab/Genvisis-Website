986e75374d695e475885ec882556d6c9df93c205
John Zaharick <johnzaharick@gmail.com>
2023-10-24 14:52:21 -0500

    Update content.jsx

--- src/assets/pages/download/content.jsx
+++ src/assets/pages/download/content.jsx
@@ -9,6 +9,12 @@ const content = {
     description:
       "Contains all the files needed to start a Genvisis project.  Includes 20 samples from the 1000G project in Illumina format.",
   },
+  "Raw Project": {
+    title: "Download Raw Project - Illumina",
+    link: "https://www.google.com/",
+    description:
+      "Contains all the files needed to start a Genvisis project.  Includes 20 samples from the 1000G project in Illumina format.",
+  },
   "Completed Project": {
     title: "Download Raw Project - Affymetrix",
     link: "https://www.yahoo.com/?guccounter=1",
