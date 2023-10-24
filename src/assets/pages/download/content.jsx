c75163977f186f395a23c80ef1ca11ec9028f78a
John Zaharick <johnzaharick@gmail.com>
2023-10-24 14:17:00 -0500

    Update content.jsx
    Testing page not loading.

--- src/assets/pages/download/content.jsx
+++ src/assets/pages/download/content.jsx
@@ -15,18 +15,7 @@ const content = {
     description:
       "Contains all the files needed to start a Genvisis project.  Includes 20 samples from the 1000G project in Affymetrix format.",
   },
-  "Sample Project Illumina": {
-    title: "Download Sample Project - Illumina",
-    link: "https://www.yahoo.com/?guccounter=1",
-    description:
-      "Contains all the files of a Genvisis project that has been run through the first four steps, which are some of the most resource intensive.  This project allows you to use Genvisis visualizations immediately.  Includes 20 samples from the 1000G project in Illumina format.",
-  },
-    "Sample Project Affymetrix - Affymetrix": {
-    title: "Download Sample Project",
-    link: "https://www.yahoo.com/?guccounter=1",
-    description:
-      "Contains all the files of a Genvisis project that has been run through the first four steps, which are some of the most resource intensive.  This project allows you to use Genvisis visualizations immediately.  Includes 20 samples from the 1000G project in Affymetrix format.",
-  },
+
 
   "Previous Versions": "Previous Versions",
 };
