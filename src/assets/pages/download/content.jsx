35e13f7a05f804668e93511e4a0537839544eec7
John Zaharick <johnzaharick@gmail.com>
2023-10-24 13:12:49 -0500

    Update content.jsx
    Added Affymetrix download options and replaced lorem ipsum with descriptions.

--- src/assets/pages/download/content.jsx
+++ src/assets/pages/download/content.jsx
@@ -3,17 +3,29 @@ const content = {
   Subtitle: "Must have Java 11 or newer installed",
 
   "Example Projects": "Example Projects",
-  "Raw Project": {
-    title: "Download Raw Project",
+  "Raw Project Illumina": {
+    title: "Download Raw Project - Illumina",
     link: "https://www.google.com/",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ipsum nibh, consequat id erat facilisis, imperdiet",
+      "Contains all the files needed to start a Genvisis project.  Includes 20 samples from the 1000G project in Illumina format.",
   },
-  "Completed Project": {
-    title: "Download Completed Project",
+  "Raw Project Affymetrix": {
+    title: "Download Raw Project - Affymetrix",
+    link: "https://www.google.com/",
+    description:
+      "Contains all the files needed to start a Genvisis project.  Includes 20 samples from the 1000G project in Affymetrix format.",
+  },
+  "Sample Project Illumina": {
+    title: "Download Sample Project - Illumina",
     link: "https://www.yahoo.com/?guccounter=1",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ipsum nibh, consequat id erat facilisis, imperdiet",
+      "Contains all the files of a Genvisis project that has been run through the first four steps, which are some of the most resource intensive.  This project allows you to use Genvisis visualizations immediately.  Includes 20 samples from the 1000G project in Illumina format.",
+  },
+    "Sample Project Affymetrix - Affymetrix": {
+    title: "Download Sample Project",
+    link: "https://www.yahoo.com/?guccounter=1",
+    description:
+      "Contains all the files of a Genvisis project that has been run through the first four steps, which are some of the most resource intensive.  This project allows you to use Genvisis visualizations immediately.  Includes 20 samples from the 1000G project in Affymetrix format.",
   },
 
   "Previous Versions": "Previous Versions",
