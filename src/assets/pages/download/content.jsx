85be50b939d34a602d22c20b95014278097017ff
John Zaharick <johnzaharick@gmail.com>
2023-10-25 10:58:39 -0500

    Update content.jsx
    Added links to zip files.

--- src/assets/pages/download/content.jsx
+++ src/assets/pages/download/content.jsx
@@ -5,25 +5,25 @@ const content = {
   "Example Projects": "Example Projects",
   "Starter Project Illumina": {
     title: "Download Starter Project - Illumina",
-    link: "https://www.google.com/",
+    link: "http://genvisis.umn.edu/downloads/Genvisis_Starter_Project_Illumina.zip",
     description:
       "Contains all the files needed to start a Genvisis project.  Includes 20 samples from the 1000G project in Illumina format.",
   },
   "Starter Project Affymetrix": {
     title: "Download Starter Project - Affymetrix",
-    link: "https://www.google.com/",
+    link: "http://genvisis.umn.edu/downloads/Genvisis_Starter_Project_Affymetrix.zip",
     description:
       "Contains all the files needed to start a Genvisis project.  Includes 20 samples from the 1000G project in Affymetrix format.",
   },
   "Sample Project Illumina": {
     title: "Download Sample Project - Illumina",
-    link: "https://www.yahoo.com/?guccounter=1",
+    link: "http://genvisis.umn.edu/downloads/Genvisis_Sample_Project_Illumina.zip",
     description:
       "Contains all the files of a Genvisis project that has been run through the first four steps, which are some of the most resource intensive.  This project allows you to use Genvisis visualizations immediately.  Includes 20 samples from the 1000G project in Illumina format.",
   },
     "Sample Project Affymetrix": {
     title: "Download Sample Project - Affymetrix",
-    link: "https://www.yahoo.com/?guccounter=1",
+    link: "http://genvisis.umn.edu/downloads/Genvisis_Sample_Project_Affymetrix.zip",
     description:
       "Contains all the files of a Genvisis project that has been run through the first four steps, which are some of the most resource intensive.  This project allows you to use Genvisis visualizations immediately.  Includes 20 samples from the 1000G project in Affymetrix format.",
   },
